f96b432eec26748ba959148e20d0b49451118b8d
Talal-Hashmi <talal.hashashmi@gmail.com>
2025-08-21 23:39:46 +0300

    Fix busgs

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { UserModule } from '../user/user.module';
+import { Post } from './entities/post.entity';
+import { User } from '../user/entities/user.entity';
 
 @Module({
+  imports: [
+    UserModule,
+    TypeOrmModule.forFeature([Post, User])
+  ],
   controllers: [PostController],
   providers: [PostService],
+  exports: [PostService]
 })
 export class PostModule {}
